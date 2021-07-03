fe05768aaaae537df7bbd483c4c594080d5d9940
mahiramzade <m.malikzade@gmail.com>
2021-07-03 16:10:28 +0400

    feat: login function (view) is created

--- user_app/hash.py
+++ user_app/hash.py
@@ -4,3 +4,6 @@ pwd_cxt = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 def hash_password(password):
     return pwd_cxt.hash(password)
+
+def verify(hashed_password, plain_password):
+    return pwd_cxt.verify(plain_password, hashed_password)
--- user_app/main.py
+++ user_app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from user_app.routers import user
+from user_app.routers import user, authentication
 from user_app import models, database
 
 app = FastAPI()
 
 models.user.Base.metadata.create_all(database.engine)
 
+app.include_router(authentication.router)
 app.include_router(user.router)
