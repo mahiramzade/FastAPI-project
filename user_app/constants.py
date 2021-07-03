4f1b9ebb4ffed8cbca637d8735e10663a309da66
mahiramzade <m.malikzade@gmail.com>
2021-07-03 20:01:25 +0400

    File for constant variables

---
+++ user_app/constants.py
@@ -0,0 +1,12 @@
+import os
+
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = os.getenv('ALGORITHM')
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
